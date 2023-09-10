40e9d91de4fbace4d1e41944b9ec862f26ee86c7
NattoHD <tubepika@gmail.com>
2023-09-10 16:57:10 -0300

    termino

--- src/main/java/cl/hdd/etc/controllers/SolicitudesController.java
+++ src/main/java/cl/hdd/etc/controllers/SolicitudesController.java
@@ -4,25 +4,55 @@ import cl.hdd.etc.entities.Solicitud;
 import cl.hdd.etc.services.SolicitudesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 public class SolicitudesController {
-
     @Autowired
     private SolicitudesService solicitudesService;
+    private boolean isTipoValid(String tipoSolicitud){
+        String tipo1 = "SolicitudCedula";
+        String tipo2= "RetiroCedula";
+        String tipo3= "SolicitudCertificadoNac";
+        String tipo4 = "SolicitudCertificadoDef";
+        return tipo1.equalsIgnoreCase(tipoSolicitud)||
+                tipo2.equalsIgnoreCase(tipoSolicitud)||
+                tipo3.equalsIgnoreCase(tipoSolicitud)||
+                tipo4.equalsIgnoreCase(tipoSolicitud);
+    }
+    private boolean esRutValido (String rut){
+        Pattern pattern = Pattern.compile("^[0-9]+-[0-9kK]{1}$");
+        Matcher matcher = pattern.matcher(rut);
+        if (!matcher.matches()) {
+            return false;
+        }
+        String[] stringRut = rut.split("-");
+        return stringRut[1].toLowerCase().equals(dv(stringRut[0]));
+    }
+    public static String dv(String rut) {
+        int M = 0, S = 1, T = Integer.parseInt(rut);
+        for (; T != 0; T = (int) Math.floor(T / 10)) {
+            S = (S + T % 10 * (9 - M++ % 6)) % 11;
+        }
+        return (S > 0) ? String.valueOf(S - 1) : "k";
+    }
 
     @PostMapping("/ingresarSolicitud")
     public ResponseEntity<Solicitud> create(@RequestBody Solicitud solicitud){
-        return ResponseEntity.ok().body(this.solicitudesService.create(solicitud));
+        if (!isTipoValid(solicitud.getTipoSolicitud())||!esRutValido(solicitud.getRut())){
+            return  ResponseEntity.badRequest().build();
+        }
+        try{
+            return ResponseEntity.ok().body(this.solicitudesService.create(solicitud));
+        }
+        catch (Exception ex) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
-
-
     @GetMapping("/verSolicitudes")
     public ResponseEntity<List<Solicitud>> getAll(){
         try{
@@ -31,5 +61,13 @@ public class SolicitudesController {
             return ResponseEntity.internalServerError().body(null);
         }
     }
-
+    @GetMapping("/verSolicitudes/{tipoSolicitud}")
+    public ResponseEntity<List<Solicitud>> filter(@PathVariable String tipoSolicitud){
+        try{
+            return ResponseEntity.ok(this.solicitudesService.filter(tipoSolicitud));
+        }
+        catch (Exception ex){
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
